950133da1f17e6a318cf4bec0083764c4bb465f3
302698648@qq.com <302698648@qq.com>
2018-01-27 23:37:52 +0800

    1

--- feijidazhan/feijidazhan.py
+++ feijidazhan/feijidazhan.py
@@ -2,7 +2,7 @@ import pygame   #pip install pygame
 from pygame.locals import *
 import time
 
-bulletSBox=[]
+bulletsBox=[]
 
 #app main entrence
 def main():
@@ -10,8 +10,8 @@ def main():
     screen=pygame.display.set_mode((480,852),0,32)
 
     #接收背景图片，飞机图片
-    background=pygame.image.load("./feiji/background.png")
-    plane=pygame.image.load("./feiji/hero1.png")
+    background=pygame.image.load('feiji/background.png').convert()
+    plane=pygame.image.load("feiji/hero1.png")
 
     #设置飞机位置
     x=480/2-100/2
@@ -34,19 +34,19 @@ def main():
 
         #监听键盘事件
         key_pressed=pygame.key.get_pressed()
-        if key_pressed[K-w] or key_pressed[K_UP]:
+        if key_pressed[K_w] or key_pressed[K_UP]:
             print("UP")
             y-=speed
-        if key_pressed[K-s] or key_pressed[K_DOWN]:
+        if key_pressed[K_s] or key_pressed[K_DOWN]:
             print("DOWN")
             y+=speed
-        if key_pressed[K-a] or key_pressed[K_LEFT]:
+        if key_pressed[K_a] or key_pressed[K_LEFT]:
             print("LEFT")
             x-=speed
-        if key_pressed[K-d] or key_pressed[K_RIGHT]:
+        if key_pressed[K_d] or key_pressed[K_RIGHT]:
             print("RIGHT")
             x+=speed
-        if key_pressed[K-SPACE]:
+        if key_pressed[K_SPACE]:
             print("FIRE")
             bullet=pygame.image.load("./feiji/bullet.png")
 
